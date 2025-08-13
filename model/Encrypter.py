d65bd9a60a66237311c9808e253d1bac559fd6c6
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2025-08-13 02:56:22 -0500

    Derive encryption key with PBKDF2 and stored salt

--- model/Encrypter.py
+++ model/Encrypter.py
@@ -1,15 +1,30 @@
 from cryptography.fernet import Fernet
 import base64
-import hashlib
+import os
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
 
 
 class Encrypter:
-    def __init__(self, key):
-        # Generate a Fernet key from the provided password/key
-        key_bytes = key.encode()
-        key_hash = hashlib.sha256(key_bytes).digest()
-        # Fernet requires a 32-byte key encoded in base64
-        self.key = base64.urlsafe_b64encode(key_hash)
+    def __init__(self, key, salt_file="salt.bin"):
+        self.password = key
+        self.salt_file = salt_file
+
+        if os.path.exists(self.salt_file):
+            with open(self.salt_file, "rb") as f:
+                salt = f.read()
+        else:
+            salt = os.urandom(16)
+            with open(self.salt_file, "wb") as f:
+                f.write(salt)
+
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+        )
+        self.key = base64.urlsafe_b64encode(kdf.derive(self.password.encode()))
         self.cipher = Fernet(self.key)
 
     def encrypt(self, data):
@@ -21,9 +36,17 @@ class Encrypter:
         return encrypted_data
 
     def decrypt(self, data):
-        # Decrypt the data
-        decrypted_data = self.cipher.decrypt(data)
-        # Try to decode to string if possible
+        with open(self.salt_file, "rb") as f:
+            salt = f.read()
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(self.password.encode()))
+        cipher = Fernet(key)
+        decrypted_data = cipher.decrypt(data)
         try:
             return decrypted_data.decode()
         except UnicodeDecodeError:
